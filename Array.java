b9d43b94c71c0381b54f6835ed84962ff2572054
babyshirisha <babyshirsha24@gmail.com>
2022-05-17 10:10:15 +0530

    added code to print Array

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+	public static void main(String args[]){ 
+    String[] arr = {"hi", "shirisha", "good", "print", "system"};
+    for(int i = arr.length-1; i >= 0; i--){
+      System.out.println(arr[i]);
+    }
+  }
+}
+
